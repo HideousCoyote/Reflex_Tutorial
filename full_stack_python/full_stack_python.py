cc0172ebe5ab5c8b38c1d2b0f35e6e8ad573904f
Mark Chambers <cmark6466@gmail.com>
2024-10-03 22:44:10 -0400

    new

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -1,39 +1,66 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
-
 from rxconfig import config
+from .ui.base import base_page
+from . import contact, blog, navigation, pages
 
 
 class State(rx.State):
     """The app state."""
+    label = "Welcome to Reflex!"
 
-    ...
-
+    def handle_input_change(self, val):
+        self.label = val
+    
+    def did_click(self):
+        print("Hello world did click")
+        return rx.redirect(navigation.routes.ABOUT_US_ROUTE)
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-left"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+    
+    my_child = rx.vstack(
+            rx.heading(State.label,  size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
+            # rx.button("About Us", on_click=State.did_click),
+            rx.link(rx.button("About Us"), href=navigation.routes.ABOUT_US_ROUTE, ),
             spacing="5",
             justify="center",
+            align="center",
+            text_align="center",
             min_height="85vh",
-        ),
-        rx.logo(),
-    )
+            id = 'my-child',
+        )
+    return base_page(my_child)
+
+
 
 
 app = rx.App()
 app.add_page(index)
+app.add_page(pages.about_page, 
+             route=navigation.routes.ABOUT_US_ROUTE)
+app.add_page(pages.pricing_page, 
+             route=navigation.routes.PRICING_ROUTE)
+app.add_page(blog.blog_post_add_page, 
+             route=navigation.routes.BLOG_POST_ADD_ROUTE),
+app.add_page(blog.blog_post_detail_page, 
+             route="/blog/[blog_id]", 
+             on_load=blog.BlogPostState.get_post_detail
+             ),
+
+app.add_page(blog.blog_post_list_page, 
+             route=navigation.routes.BLOG_POSTS_ROUTE,
+             on_load=blog.BlogPostState.load_posts
+             )
+
+app.add_page(contact.contact_page, 
+             route=navigation.routes.CONTACT_US_ROUTE)
+app.add_page(contact.contact_entries_list_page, 
+             route=navigation.routes.CONTACT_ENTRIES_ROUTE,
+             on_load=contact.ContactState.list_entries
+             )
---
+++ full_stack_python/navigation/state.py
@@ -0,0 +1,19 @@
+import reflex as rx
+from . import routes
+
+class NavState(rx.State):
+
+    def to_home(self):
+        return rx.redirect(routes.HOME_ROUTE)
+    def to_about_us(self):
+        return rx.redirect(routes.ABOUT_US_ROUTE)
+    def to_blog(self):
+        return rx.redirect(routes.BLOG_POSTS_ROUTE)
+    def to_blog_add(self):
+        return rx.redirect(routes.BLOG_POST_ADD_ROUTE)
+    def to_blog_create(self):
+        return self.to_blog_add()
+    def to_pricing(self):
+        return rx.redirect(routes.PRICING_ROUTE)
+    def to_contact(self):
+        return rx.redirect(routes.CONTACT_US_ROUTE)
